960856550910ee5285caa0b97de4c6fea6d9ef30
Hannah Luebbering <61786322+hluebbering@users.noreply.github.com>
2021-01-18 02:10:54 -0600

    Add files via upload

---
+++ seaborn/bar-plot.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Load results.csv here:
+df = pd.read_csv('results.csv')
+print(df)
+
+sns.barplot(
+	data=df,
+	x='Gender',
+	y='Mean Satisfaction'
+)
+
+plt.show()
